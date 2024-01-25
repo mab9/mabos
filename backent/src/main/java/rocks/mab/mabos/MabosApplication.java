1b017fc2530039f09988668c5dae1d1a65462876
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-01-25 13:56:27 +0100

    Init project mabos

---
+++ backent/src/main/java/rocks/mab/mabos/MabosApplication.java
@@ -0,0 +1,13 @@
+package rocks.mab.mabos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MabosApplication.class, args);
+	}
+
+}
---
+++ backent/src/test/java/rocks/mab/mabos/MabosApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.mab.mabos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
