f1cdbf8b7d6a3d91b4535458869fb6307e5c195e
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-01-25 14:07:51 +0100

    Add angular material and start impl toolbar

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavContainer, MatSidenavModule} from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, MatButtonModule, MatSidenavModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavContainer, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
