e5cc93ccb3615dff03789c2f576f9208452d0bf4
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-01-26 10:39:55 +0100

    Do some styling and add home card component for overview

--- spa/src/app/home/home.component.ts
+++ spa/src/app/home/home.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardHeader,
+  MatCardSubtitle,
+  MatCardTitle
+} from "@angular/material/card";
+import {MatList, MatListItem} from "@angular/material/list";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardActions,
+    MatCardTitle,
+    MatCardSubtitle,
+    MatList,
+    MatListItem,
+    MatIcon
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
