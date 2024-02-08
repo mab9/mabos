1a4d35553e15939113169c44267ff3d9f90d07a1
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-02-08 14:23:47 +0100

    Start creating new main detail abo view

---
+++ spa/src/app/components/abos-main-detail/abos-main-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abos-main-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './abos-main-detail.component.html',
+  styleUrl: './abos-main-detail.component.scss'
+})
+export class AbosMainDetailComponent {
+
+}
