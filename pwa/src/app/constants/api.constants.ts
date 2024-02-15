ab6eb31678a5df8e1ce64ce42737663bca6e2c6e
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-02-15 09:46:14 +0100

    Remove all the PWA code because offline mode is not working out of the box and I don't want to spend much time for it at the moment.

--- pwa/src/app/constants/api.constants.ts
+++
@@ -1,5 +0,0 @@
-
-export const  ApiConstants = {
-    API_ABOS : "/api/abos",
-    API_USERS : "/api/users",
-}
--- pwa/src/app/model/period.enum.ts
+++
@@ -1,7 +0,0 @@
-export enum Period {
-  WEEK = 'WEEK',
-  YEAR = 'YEAR',
-  MONTH = 'MONTH',
-  QUARTER_YEAR = 'QUARTER_YEAR',
-  HALF_YEAR = 'HALF_YEAR',
-}
