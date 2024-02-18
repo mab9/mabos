68e4c8c88a4514a6f495c64364b1f53cbb09c5ee
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-02-18 12:20:15 +0100

    Update spa env configs for server deployment

--- spa/src/environments/environment.prod.ts
+++ spa/src/environments/environment.prod.ts
@@ -1,12 +1,15 @@
+
+const host = 'https://abos.mab.rocks'
+
 export const environment = {
   production: true,
-  backendUrl : 'http://localhost:8080',
+  backendUrl : host + ':8443',
   keycloak : {
-    config_url: 'http://localhost:8180/',
+    config_url:  host + ':2096/',
     config_realm: 'mabos-realm',
     config_clientId: 'mabos-public-client',
     initOptions_pkceMethod: "S256", // todo check why this property is not working
-    initOptions_redirectUri: 'http://localhost:4200/dashboard',
-    logout_redirectUri: 'http://localhost:4200/landing',
+    initOptions_redirectUri:  host + '/dashboard',
+    logout_redirectUri:  host + '/landing',
   }
 };
