2029d4f202342e603516c0ac7bb436d65c35f8f2
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-02-21 08:55:16 +0100

    Adjust spa keycloak url

--- spa/src/environments/environment.prod.ts
+++ spa/src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
 
-const host = 'https://iam.mab.rocks'
+const host = 'https://abos.mab.rocks'
+const iam = 'https://iam.mab.rocks'
 
 export const environment = {
   production: true,
   backendUrl : host + ':8443',
   keycloak : {
-    config_url:  host + '/',
+    config_url:  iam + '/',
     config_realm: 'mabos-realm',
     config_clientId: 'mabos-public-client',
     initOptions_pkceMethod: "S256", // todo check why this property is not working
