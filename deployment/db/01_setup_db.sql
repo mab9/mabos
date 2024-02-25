e627e951002a378a55b9fabd982105de10efd662
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-02-25 11:28:14 +0100

    Update db setup script - remove data drop on execution

--- deployment/db/01_setup_db.sql
+++ deployment/db/01_setup_db.sql
@@ -1,9 +1,11 @@
 
-drop schema if exists keycloak;
+-- uncomment for developing and testing
+-- drop schema if exists keycloak;
 CREATE SCHEMA keycloak;
 GRANT ALL ON SCHEMA keycloak TO "mabos-db-admin";
 
-drop SCHEMA if exists mabos;
+-- uncomment for developing and testing
+-- drop SCHEMA if exists mabos;
 CREATE SCHEMA if not exists mabos;
 GRANT ALL ON SCHEMA mabos TO "mabos-db-admin";
 
