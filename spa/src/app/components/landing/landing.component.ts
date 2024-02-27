9b7e21f01e201adfeb99769e193131adb4b68be0
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-02-27 21:43:27 +0100

    Update landing page and pwa styles and icons

--- spa/src/app/components/landing/landing.component.ts
+++ spa/src/app/components/landing/landing.component.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
-export class LandingComponent {
+export class LandingComponent implements AfterViewInit {
 
+  isSpinning = false;
+  text: string[] = [];
+  triggerStartTextAnimation = false;
+
+  constructor() {
+    this.startSpin();
+    const phrase = "Start tracking your abo expenses now!";
+    this.text = phrase.split(' ');
+    // Start the text animation just before the logo animation finishes
+    setTimeout(() => {
+      this.startTextAnimation(); // This could simply set a flag to start the animation
+    }, 1500); // Adjust the time as needed
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  startTextAnimation() {
+    this.triggerStartTextAnimation = false; // reset in case of animation shall run again.
+    this.triggerStartTextAnimation = true;
+  }
+
+  startSpin() {
+    this.isSpinning = false; // reset in case of animation shall run again.
+    this.isSpinning = true;
+  }
 }
