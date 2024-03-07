4afe3a8a06a04188ffa32a21e956b03f186d45a3
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-03-07 15:34:05 +0100

    Implement simple feature flag solution

--- backent/src/main/resources/db/migration/V5__create_table_feature_flags.sql
+++ backent/src/main/resources/db/migration/V5__create_table_feature_flags.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS feature_flags (
                      user_email VARCHAR(100) NOT NULL,
                      feature VARCHAR(100) NOT NULL,
                      flag BOOLEAN NOT NULL DEFAULT false,
-                     CONSTRAINT fk_users
+                     CONSTRAINT fk_feature_flags_users
                          FOREIGN KEY(user_email)
                              REFERENCES users(email)
 );
 
-INSERT INTO feature_flags (id, user_email, feature, flag) VALUES (1, 'mab9@mab.rocks', 'NAV', false);
+--INSERT INTO feature_flags (id, user_email, feature, flag) VALUES (1, 'mab9@mab.rocks', 'NAV', false);
