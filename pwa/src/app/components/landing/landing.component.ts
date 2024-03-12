40c40b0054541e5f54489bd3a66c46e17c2e102f
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-03-12 18:18:51 +0100

    Start creating fancy feature section on landing page

--- pwa/src/app/components/landing/landing.component.ts
+++ pwa/src/app/components/landing/landing.component.ts
@@ -1,13 +1,17 @@
 import {Component} from '@angular/core';
 import {NgForOf, NgIf} from "@angular/common";
 import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
     NgForOf,
-    NgIf
+    NgIf,
+    MatIcon,
+    MatIconButton
   ],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
@@ -19,4 +23,8 @@ export class LandingComponent {
 
   protected readonly BreakpointObserver = BreakpointObserver;
   protected readonly Breakpoints = Breakpoints;
+
+  scrollToFeatures() {
+      document.getElementById('features-container')?.scrollIntoView({behavior: 'smooth'});
+  }
 }
