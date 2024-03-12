a756ce3c02e2be26c0a79cd7b76e7b9e44e9ff28
Marc-Antoine Brülhart <marcantoine.bruelhart@gmail.com>
2024-03-12 14:54:36 +0100

    Quick fixes for demo case at SEO academy

--- backent/src/main/resources/db/migration/V2__update_tables_with_reminder_columns.sql
+++ backent/src/main/resources/db/migration/V2__update_tables_with_reminder_columns.sql
@@ -1,4 +1,4 @@
-ALTER TABLE mabos.abo ADD COLUMN is_exp_reminder_active BOOLEAN NOT NULL DEFAULT TRUE;
-ALTER TABLE mabos.abo ADD COLUMN exp_reminder_period VARCHAR(30) NOT NULL DEFAULT 'WEEK';
-ALTER TABLE mabos.abo ADD COLUMN exp_reminder_period_amounts SMALLINT NOT NULL DEFAULT 1;
+ALTER TABLE abo ADD COLUMN is_exp_reminder_active BOOLEAN NOT NULL DEFAULT TRUE;
+ALTER TABLE abo ADD COLUMN exp_reminder_period VARCHAR(30) NOT NULL DEFAULT 'WEEK';
+ALTER TABLE abo ADD COLUMN exp_reminder_period_amounts SMALLINT NOT NULL DEFAULT 1;
 
--- backent/src/main/resources/db/migration/V4__update_table_remove_some_not_null_constraint.sql
+++ backent/src/main/resources/db/migration/V4__update_table_remove_some_not_null_constraint.sql
@@ -1,7 +1,7 @@
-ALTER TABLE mabos.abo ALTER COLUMN start_date DROP NOT NULL;
-ALTER TABLE mabos.abo ALTER COLUMN is_exp_reminder_active DROP NOT NULL;
-ALTER TABLE mabos.abo ALTER COLUMN exp_reminder_period DROP NOT NULL;
-ALTER TABLE mabos.abo ALTER COLUMN exp_reminder_period_amounts DROP NOT NULL;
+ALTER TABLE abo ALTER COLUMN start_date DROP NOT NULL;
+ALTER TABLE abo ALTER COLUMN is_exp_reminder_active DROP NOT NULL;
+ALTER TABLE abo ALTER COLUMN exp_reminder_period DROP NOT NULL;
+ALTER TABLE abo ALTER COLUMN exp_reminder_period_amounts DROP NOT NULL;
 
 
 
